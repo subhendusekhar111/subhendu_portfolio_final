78eebfcb0227bab515301fc1491b03ec44eb1e9a
Rahul1582 <rahulkpatro@gmail.com>
2020-07-24 01:16:39 +0530

    updated code

--- js/main.js
+++ js/main.js
@@ -455,3 +455,19 @@ $(window).on('load', function() { // makes sure the whole site is loaded
   })
 
 
+  $(document).ready(function () {
+    var owl = $(".timeline-carousel");
+  
+    owl.owlCarousel({
+      itemsCustom: [
+        [0, 1],
+        [500, 2],
+        [768, 3],
+        [1024, 4],
+        [1350, 5],
+        [2000, 6]
+      ],
+      navigation: true
+    });
+  });
+  
